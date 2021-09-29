b561e904db1f203e3ed113d07a065e65c1a0534c
jrrohrer <jessie.rohrer@gmail.com>
2021-09-29 12:38:59 -0400

    commit tree f28e0404fa7f1695dd93ccd20f2ac12af8c55daa

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert("Stop poking me :(")} />
   </div>,
   document.getElementById('root')
 );
